76ef17ce298d6cfba8a5f02d95ff186bc0f48491
jilee <jilee@jileeui-MacBook-Pro.local>
2018-08-09 21:36:39 +0900

    axios setting

---
+++ client/src/services/AuthenticationService.js
@@ -0,0 +1,12 @@
+import Api from '@services/Api'
+
+export default {
+    register(credentials) {
+        return Api().post('register', credentials)
+    }
+}
+
+// AutenticationService.register()({
+//     email : 'chopeoghk@gmail.com',
+//     password: 'Manager1!'
+// })
